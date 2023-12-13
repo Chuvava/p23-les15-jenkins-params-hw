f48871bfc1a978a8bb9cca7fdb260e95943ebed4
Aleksandr Efimov <aleksandr.efimov94@mail.ru>
2023-12-13 01:31:40 +0100

    added get system params

--- src/test/java/com/aefimov/BaseTest.java
+++ src/test/java/com/aefimov/BaseTest.java
@@ -14,9 +14,9 @@ public class BaseTest {
     @BeforeAll
     static void beforeAll() {
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy="eager";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.browserSize = System.getProperty("browser_size", "1920x1080");
+        Configuration.remote = System.getProperty("remote_address");
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
